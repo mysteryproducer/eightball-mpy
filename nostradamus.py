af4a48830048ed5bd6ce56ffdbd0c527706f1ba4
Chris Arnold <mysteryproducer@gmail.com>
2025-05-08 17:39:59 +0800

    nostradamus functioning; to be improved

--- nostradamus.py
+++ nostradamus.py
@@ -1,14 +1,6 @@
-#from gen import Generator
-import random
+from gen import TemplateGenerator
 
-class Nostradamus():#Generator):
+class Nostradamus(TemplateGenerator):
     
-    _conditions=list()
-    _max=0
     def __init__(self):
-        file = open('dsm5.txt')
-        _conditions=file.read().split("\n")
-        _max=len(_conditions)
-    
-    def generate(self):
-        return _conditions[random.randint(0,_max)]
+        super().__init__('assets/nostradamus.txt')
